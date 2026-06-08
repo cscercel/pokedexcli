192e156a8de60c536346c0e086c77b1888490d91
Cercel, Cristian <cristian.cercel@outlook.com>
2026-06-08 14:44:14 -0400

    better help callback

--- command_help.go
+++ command_help.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
-func callbackHelp() error {
+func commandHelp() error {
+	fmt.Println()
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
-	fmt.Println("")
-	fmt.Println("help: Displays a help message")
-	fmt.Println("exit: Exit the Pokedex")
-
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
 	return nil
 }
