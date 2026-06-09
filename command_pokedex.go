844b716922d1a5bd2cc67e9f3189a962d6611b94
Cercel, Cristian <cristian.cercel@outlook.com>
2026-06-08 22:16:35 -0400

    done

---
+++ command_pokedex.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func callbackPokedex(cfg *config, args ...string) error {
+	fmt.Println("Pokemon in Pokedex:")
+	for _, pokemon := range cfg.caughtPokemon {
+		fmt.Printf("Name: %s\n", pokemon.Name)
+	}
+
+	return nil
+}
